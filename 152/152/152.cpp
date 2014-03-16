623cbcfe0c7547db189a35b99d86e769d2376f5a
shajinzheng <shajinzheng1993@gmail.com>
2014-03-16 14:58:22 +0800

    lab

--- 152/152/152.cpp
+++ 152/152/152.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 #define  LOCAL
-#define MAXM 5100
+#define MAXM 6000
 double tree[MAXM][3];
 int results[10];
 double delta;
@@ -12,19 +12,21 @@ int main()
 {
 #ifdef LOCAL
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif // LOCAL
+	freopen("a.txt", "w", stdout);
+#endif
 	int iter = 0;
 	memset(results, 0, 10*sizeof(int));
 	scanf("%lf%lf%lf", &tree[iter][0], &tree[iter][1], &tree[iter][2]);
 	iter++;
-	while(tree[iter - 1][0] != 0)
+	while(1)// 将全部的tree读进来
 	{
+		if (tree[iter - 1][0] + tree[iter - 1][1] + tree[iter - 1][2] < 1e-9)
+			break;
 		scanf("%lf%lf%lf", &tree[iter][0], &tree[iter][1], &tree[iter][2]);
 		iter++;
 	}
 	int i, j;
-	for (i = 0; i < iter; i++)
+	for (i = 0; i < iter; i++)//双重循环对于每一个tree，找到离它最近的tree的距离
 	{
 		double closest = 100000000;
 		for (j = 0; j < iter; j++)
@@ -35,11 +37,12 @@ int main()
 				closest = (closest > delta)?delta:closest;
 			}
 		}
-		int idx = floor(closest + 0.1);
+		int idx = floor(closest);
 		if ((idx >= 0)&&(idx <= 9))
 			results[idx]++;
 	}
 	for (i = 0; i < 10; i++)
-		printf("    %d", results[i]);
+		printf("%4d", results[i]); //注意不能写成printf("   %d", results[i]);是不对的
+	printf("\n");
 	return 0;
 }
--- 573/573/573.cpp
+++ 573/573/573.cpp
@@ -9,26 +9,20 @@ int main()
 #ifdef LOCAL
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-#endif // LOCAL
+#endif
 	scanf("%f%f%f%f", &h, &u, &d, &f);
 	while (h != 0)
 	{
 		float speed = u, h_t = 0, delta = u * f/100.0;
 		int iter = 0, flagSuccess = 0, flagFail = 0;
-		while (1)
+		while ((!flagFail)&&(!flagSuccess))
 		{
 			iter++;
 			speed = (speed > 0)?speed:0;
 			if (h_t + speed > h)
-			{
 				flagSuccess = 1;
-				break;
-			}
 			else if (h_t + speed - d < 0)
-			{
 				flagFail = 1;
-				break;
-			}
 			h_t = h_t + speed - d;
 			speed -= delta;
 		}
